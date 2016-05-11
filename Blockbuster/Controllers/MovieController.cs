ad033174619c0f8fbc359960918205ba47ff6eec
NapoleonRSA <schwartz.napoleon@gmail.com>
2016-05-11 10:06:44 +0200

    Project start

---
+++ Blockbuster/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blockbuster.Controllers
+{
+    public class MovieController : Controller
+    {
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public JsonResult Create(CreateMovieModel model)
+        {
+            var movies = model.ToDomain();
+            using (var movieRepo = new MovieRepository())
+            {
+                movieRepo.Create(movies);
+            }
+
+            return new JsonResult { Data = new { IsOk = true } };
+        }
+    }
+}
